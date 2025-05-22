68c6b9bca94a07fbb4861ab35c1fc68ce8bee0ee
Jesus Lara <jblara25@gmail.com>
2025-05-22 22:03:54 +0800

    first commit: typescript masterclass sessio

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(5, 10)); // Output: 15
+console.log(addNumbers("text", 10));
---
+++ index.ts
@@ -0,0 +1,6 @@
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
+
+console.log(addNumbers(5, 10)); // Output: 15
+console.log(addNumbers("text", 10));
