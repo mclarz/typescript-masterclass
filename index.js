947699fe0fb59baaba753d93a9c046327f6e4759
Jesus Lara <jblara25@gmail.com>
2025-05-23 00:05:03 +0800

    declration, anotation and inference

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 "use strict";
-/**
- *
- */
-let message = "Hello, Typescript!";
-let age = 42;
-let isStudent = true;
-let fetched = null;
-let user = undefined;
-let largeNumber = 9007199254740991n;
-let unique = Symbol("uniqueSymbol");
+function multplyByTwo(number) {
+    if (typeof number === "number") {
+        return number * 2;
+    }
+    return "Invalid input: expected a number";
+}
+// anotation variable or assign type
+let firstname = "Mark";
+let age = 20;
+let today = new Date();
+let unique = Symbol();
+function addNumbers(a, b) {
+    return a + b;
+}
+// inference - judges or infers the type
+let finalResult = addNumbers(2, 3);
--- index.ts
+++ index.ts
@@ -1,10 +1,44 @@
+function multplyByTwo(number: unknown) {
+  if (typeof number === "number") {
+    return number * 2;
+  }
+  return "Invalid input: expected a number";
+}
 /**
+ * Error;
+ * String;
+ * Number;
+ * Date;
+ * Symbol;
+ * Boolean
+ * Weakmap
+ * Weakset
+ * Bigint
+ * RegExp
+ * Function
+  * Map
+  * Set
+  * Array
+  * Object
  *
  */
-let message: string = "Hello, Typescript!";
-let age: number = 42;
-let isStudent: boolean = true;
-let fetched: null = null;
-let user: undefined = undefined;
-let largeNumber: bigint = 9007199254740991n;
-let unique: symbol = Symbol("uniqueSymbol");
+
+// Declaration - when youre declaring a type
+type CustomString = string;
+type CustomNumber = number;
+type CustomDate = Date;
+type CustomSymbol = Symbol;
+
+// anotation variable or assign type
+let firstname: CustomString = "Mark";
+let age: CustomNumber = 20;
+let today: CustomDate = new Date();
+let unique: CustomSymbol = Symbol();
+
+
+function addNumbers(a:number, b:number) {
+  return a + b;
+}
+// inference - judges or infers the type
+let finalResult = addNumbers(2, 3);
+
