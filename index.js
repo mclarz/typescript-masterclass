1fe90b1e4ddb285b549eecc7d039cbc73adeef03
Jesus Lara <jblara25@gmail.com>
2025-05-27 22:56:02 +0800

    working with objects

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 "use strict";
-let city = "New York"; // string
-let population = 8400000; // number
-const age = 32; // number
-let oldAge = 79; // number
-let newAge = oldAge; // number
-let data = new Map(); // map or any
-let score = [90, 86, 100]; // object or array
-/**
- * Are the following statements valid
- * Check if below lines of code are valid as per TypeScript or not without uncommenting them
- *  */
-age = 85; // not valid
-score.push(10); // true
-score.push("New Score"); // false should be a number
-let customAge = 50; // false should be 79
-let primitive = new Date(); // false should string or number or boolean
-let years = []; // true it has type of array
+let person = {
+    name: "Mark",
+    age: 30,
+};
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+};
+car = [];
+car = () => { };
+let newCar = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+};
+newCar = [];
--- index.ts
+++ index.ts
@@ -1,24 +1,21 @@
-let city = "New York"; // string
-let population = 8400000; // number
-const age = 32; // number
-let oldAge = 79 as const; // number
-let newAge = oldAge; // number
-let data = new Map(); // map or any
-let score = [90, 86, 100]; // object or array
-type Primitive = string | number | boolean; // string | number | boolean
-type CustomName = "John" extends string ? string : "John"; // string
-type CustomAge = typeof newAge extends number ? 79 : number; // 79
-type CheckData = typeof data extends Object ? true : false; //  true
-type CheckScore = typeof score extends never ? {} : []; // array
+let person = {
+  name: "Mark",
+  age: 30,
+};
 
-/**
- * Are the following statements valid
- * Check if below lines of code are valid as per TypeScript or not without uncommenting them
- *  */
+let car: Object = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2020,
+};
 
-age = 85; // not valid
-score.push(10); // true
-score.push("New Score"); // false should be a number
-let customAge: CustomAge = 50; // false should be 79
-let primitive: Primitive = new Date(); // false should string or number or boolean
-let years: CheckScore = []; // true it has type of array
+car = [];
+car = () => {};
+
+let newCar: { make: string; model: string; year: number } = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2020,
+};
+
+newCar = [];
