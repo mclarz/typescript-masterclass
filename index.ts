2e9557d3107924788b7cc60afccc376a9850728e
Jesus Lara <jblara25@gmail.com>
2025-07-19 21:57:49 +0800

    learning nested objects

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
+type Author = {
+  name: string;
+  age: number;
+  email: string;
+};
+
 type Post = {
   title: string;
   content: string;
   date: Date;
+  author: Author;
 };
 
 let post: Post = {
   title: "This is post title",
   content: "This is content of the post",
   date: new Date(),
+  author: {
+    name: "John",
+    age: 30,
+    email: "john@doe.com",
+  },
 };
