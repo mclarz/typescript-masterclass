f926f10065fa9cbca8d10c90726cd923153c2c68
Jesus Lara <jblara25@gmail.com>
2025-07-21 21:21:27 +0800

    intersection unions

--- index.ts
+++ index.ts
@@ -1,38 +1,18 @@
-type NetworkLoadingState = {
-  state: "loading";
+type Dog = {
+  name: string;
+  barks: boolean;
+};
+type Cat = {
+  name: string;
+  purrs: boolean;
+  color: string;
 };
 
-type NetworkFailedState = {
-  state: "failed";
-  code: number;
+type HybridAnimal = Dog & Cat;
+
+let HyBridAnimal: HybridAnimal = {
+  name: "Hybrid",
+  color: "white",
+  purrs: true,
+  barks: false,
 };
-
-type NetworkSuccessState = {
-  state: "success";
-  response: {
-    title: string;
-    duration: number;
-    summary: string;
-  };
-};
-
-type NetworkState =
-  | NetworkLoadingState
-  | NetworkFailedState
-  | NetworkSuccessState;
-
-function logger(state: NetworkState) {
-  switch (state.state) {
-    case "loading":
-      console.log("Loading...");
-      break;
-    case "failed":
-      console.error(`Failed with code: ${state.code}`);
-      break;
-    case "success":
-      console.log(
-        `Success! Title: ${state.response.title}, Duration: ${state.response.duration}, Summary: ${state.response.summary}`
-      );
-      break;
-  }
-}
