25a030422f110401918f6043d2a9dbe16f3a5710
Jesus Lara <jblara25@gmail.com>
2025-07-21 21:25:54 +0800

    exam object types

--- index.ts
+++ index.ts
@@ -1,18 +1,38 @@
-type Dog = {
+// alias of caterer
+type Caterer = {
   name: string;
-  barks: boolean;
-};
-type Cat = {
-  name: string;
-  purrs: boolean;
-  color: string;
+  address: string;
+  phone: number;
 };
 
-type HybridAnimal = Dog & Cat;
+// alias of seat
+type Seat = {
+  [seatNumber: string]: string;
+};
 
-let HyBridAnimal: HybridAnimal = {
-  name: "Hybrid",
-  color: "white",
-  purrs: true,
-  barks: false,
+// alias of airplane
+type Airplane = {
+  model: string;
+  flightNumber: string;
+  timeOfDeparture: Date;
+  timeOfArrival: Date;
+  caterer: Caterer;
+  seats: Seat;
+};
+
+let airplane = {
+  model: "Airbus A380",
+  flightNumber: "A2201",
+  timeOfDeparture: new Date(),
+  timeOfArrival: new Date(),
+  caterer: {
+    name: "Special Food Ltd",
+    address: "484, Some Street, New York",
+    phone: 7867856751,
+  },
+  seats: {
+    A1: "John Doe",
+    A2: "Mark Doe",
+    A3: "Sam Doe",
+  },
 };
